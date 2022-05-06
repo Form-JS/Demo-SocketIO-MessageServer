114be0fb64cc7a0206067d905505f82ef8adb3ce
Aurélien <aurelien.strimelle@bstorm.be>
2022-05-06 14:31:45 +0200

    Using UserToken

--- index.js
+++ index.js
@@ -21,32 +21,57 @@ const io = require("socket.io")(server, {
 });
 
 io.on('connection', (socket) => {
-  console.log('User connected');
+  const userToken = socket.handshake.auth.token;
 
+  // Info de connection et déconnection dans la console serveur
+  console.log('User connected -> ', userToken);
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('User disconnected -> ', userToken);
   });
 
   // Réaction a la reception de message
   socket.on('message', (msg) => {
-    console.log('New Message: ', msg);
-    io.emit('message', msg);
+    console.log('New Message: ', userToken, msg);
+
+    // ↓ Envoi vers tout les client connecté
+    // io.emit('message', `${userToken}: ${msg}`);
+
+    // ↓ Envoi vers tout les client sauf l'emetteur
+    socket.broadcast.emit('message', `${userToken}: ${msg}`);
+
+    // ↓ Envoi vers uniquement l'emetteur
+    socket.emit('message', `Moi: ${msg}`);
   });
 
   // Mecanisme des groupes
   socket.on('groupJoin', (group) => {
-    console.log('Join Group: ', group);
-    io.to(group).emit('message', `[${group}] Un nouveau est là :o`);
+    console.log('Join Group: ', group, userToken);
     socket.join(group);
+
+    // ↓ Envoi vers tout les client du group sauf l'emetteur
+    socket.broadcast.to(group).emit('message', `[${group}] ${userToken} a rejoint le groupe`);
   });
+
   socket.on('groupLeave', (group) => {
-    console.log('Leave Group: ', group);
+    console.log('Leave Group: ', group, userToken);
     socket.leave(group);
-    io.to(group).emit('message', `[${group}] On a un fuyard`);
+
+    // ↓ Envoi vers tout les client du group sauf l'emetteur
+    socket.broadcast.to(group).emit('message', `[${group}] ${userToken} est parti :(`);
   });
+
   socket.on('groupMessage', (group, msg) => {
     console.log('New Group Message: ', group, msg);
-    io.to(group).emit('message', `[${group}] ${msg}`);
+
+    // ↓ Envoi vers tout les client connecté au groupe (emetteur compris)
+    // io.to(group).emit('message', `[${group}] ${userToken}: ${msg}`);
+
+    // ↓ Envoi vers tout les client du group sauf l'emetteur
+    socket.broadcast.to(group).emit('message', `[${group}] ${userToken}: ${msg}`);
+
+    // ↓ Envoi vers uniquement l'emetteur (Pas de lien avec le groupe)
+    socket.emit('message', `[${group}] Moi: ${msg}`);
+
   });
 });
 
